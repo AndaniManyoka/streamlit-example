8aa56b3422dd4968a64bb8d93b1ae57d18919091
AndaniManyoka <130055564+AndaniManyoka@users.noreply.github.com>
2023-04-14 18:44:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import streamlit as st
 
+
 st.title("My first Streamlit app")
 st.write("Streamlit is fun")
 
@@ -11,3 +12,6 @@ st.write('Welcome to my Streamlit app!')
 
 x = st.slider('Select a value for x', 0, 10)
 st.write('You selected:', x)
+
+cars = pd.read_csv("cars",sep=";")
+st.bar_chart(cars, x="col1", y="col2")
