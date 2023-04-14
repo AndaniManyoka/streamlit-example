edf6f25440814b250d1089ad04021915e098ca0e
AndaniManyoka <130055564+AndaniManyoka@users.noreply.github.com>
2023-04-14 18:33:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,6 @@
 
-import numpy as np
 import pandas as pd
 import streamlit as st
 
-
-st.title['Overview of Cars']
-st.write['This project explores cars']
-
-df = pd.read_csv('cars',sep=';',skiprows=[1]
-
-#df2 = df.groupby('Origin')['Cars'].count().reset_index()
-
-st.write('My First Table')
-st.table(df)
+st.title("My first Streamlit app")
+st.write("Streamlit is fun")
