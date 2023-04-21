f0af8374b1363439a1908e308814719c6c11e730
AndaniManyoka <130055564+AndaniManyoka@users.noreply.github.com>
2023-04-21 14:09:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,10 +16,9 @@ st.write('You selected:', x)
 
 ##Visualizinng the airlines data frame
 # Define the airlines data
-airlines = [...]
 
 # Creating the dataframe and set column names
-airlines = pd.DataFrame(airlines, columns=('Airline ID', 'Name', 'Alias', 'IATA', 'ICAO', 'Callsign', 'Country', 'Active'))
+airlines = pd.DataFrame("airlines.dat", columns=('Airline ID', 'Name', 'Alias', 'IATA', 'ICAO', 'Callsign', 'Country', 'Active'))
 
 # Displaying the dataframe in Streamlit
 st.dataframe(data=airlines, width=None, height=None)
