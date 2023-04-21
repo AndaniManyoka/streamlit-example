547031bb21b11afb59573ef54e154a6a52667acd
AndaniManyoka <130055564+AndaniManyoka@users.noreply.github.com>
2023-04-21 15:56:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,13 +14,14 @@ st.write('Welcome to my Streamlit app!')
 x = st.slider('Select a value for x', 0, 10)
 st.write('You selected:', x)
 
+st.write("Airlines dataframe")
 ##Visualizinng the airlines data frame
 # Define the airlines data
 airlines =pd.read_csv('airlines1.csv',sep = ';')
 airlines.columns =['Airline ID','Name','Alias','IATA','ICAO','Callsign','Country','Active']
 
 # Displaying the dataframe in Streaml
-st.write("Airlines dataframe")
+
 st.table(airlines)
 
 
