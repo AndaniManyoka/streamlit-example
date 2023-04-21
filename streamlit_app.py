0c436058b774ae1cfb24520791311e3e462a48c1
AndaniManyoka <130055564+AndaniManyoka@users.noreply.github.com>
2023-04-21 13:11:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 import streamlit as st
+import numpy as np
 
 
 st.title("My first Streamlit app")
@@ -12,6 +13,9 @@ st.write('Welcome to my Streamlit app!')
 
 x = st.slider('Select a value for x', 0, 10)
 st.write('You selected:', x)
+
+Airline = pf.Dataframe(airlines, columns = ('Airline ID','Name','Alias','IATA','ICAO','Callsign','Country','Active')
+st.dataframe(Airlines)
 
 
 
