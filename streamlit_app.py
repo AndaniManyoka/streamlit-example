f46d896dda7cdf602ae7e8e61b3711085d5bd2d7
AndaniManyoka <130055564+AndaniManyoka@users.noreply.github.com>
2023-04-24 10:03:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ df.columns = ['Airline ID','Name','Alias','IATA','ICAO','Callsign','Country','Ac
 
 df2 = df.groupby('Country')['Name'].count().reset_index()
 st.write('Number of airlines per Country')
-st.bar_chart(df2,x='Country',y='Name')
+st.bar_chart(df2,x='Country',y='Number of airlines')
 
 
 
