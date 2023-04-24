2dfcaa1d3867fcaedcf798a834ff160d18ddd9ef
AndaniManyoka <130055564+AndaniManyoka@users.noreply.github.com>
2023-04-24 10:38:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ st.bar_chart(df2,x='Country',y='Name')
 airpots = pd.read_csv('airports1.csv', sep=",", encoding='utf-8')
 airpots.columns = ['Airport ID','Name','City','Country','IATA','ICAO','Latitude','Longitude','Altitude','Timezone','DST','Tz database time zone',
 'Type','Source']
-airpotsDist = airpots[['Latitude','Longitude']].rename(columns={'Latitude': 'latitude'})
+airpotsDist = airpots[['Latitude','Longitude']].rename(columns={'Latitude': 'latitude','Longitude':'longitude'})
 st.write('Distribution of the airports across the globe')
 st.map(data=airpotsDist)
 
